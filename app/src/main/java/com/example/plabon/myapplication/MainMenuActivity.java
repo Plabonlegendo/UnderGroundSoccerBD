75dc60c2bca3eb88398cd030d9a4d225c0ed62f2
Plabonlegendo <plabonshaha.23.41.csedu@gmail.com>
2019-01-16 00:01:19 +0600

    event done

--- app/src/main/java/com/example/plabon/myapplication/MainMenuActivity.java
+++ app/src/main/java/com/example/plabon/myapplication/MainMenuActivity.java
@@ -1,14 +1,30 @@
 package com.example.plabon.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.fasterxml.jackson.databind.deser.std.FromStringDeserializer;
 
 public class MainMenuActivity extends Activity {
+
+    Button EventHostMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        EventHostMenu = findViewById(R.id.EventHostButton);
+
+        EventHostMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this,Frontpage.class));
+            }
+        });
     }
 
 }
